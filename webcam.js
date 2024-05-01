634061861111c1671ac844aa0a7380ebb2d8b518
cloudy-444 <logan.oppelt05@gmail.com>
2024-05-01 14:48:22 -0400

    test test

---
+++ webcam.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let video = document.getElementById("vid");
+    let mediaDevices = navigator.mediaDevices;
+    vid.muted = true;
+    // Accessing the user camera and video.
+    mediaDevices
+        .getUserMedia({
+            video: true,
+            //audio: true,
+        })
+        .then((stream) => {
+            alert('What do you see?');
+            // Changing the source of video to current stream.
+            video.srcObject = stream;
+            video.addEventListener("loadedmetadata", () => {
+                video.play();
+            });
+        })
+        .catch(alert);
+});
+
